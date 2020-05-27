0e08c8e936fb6b871a9042afaa32ae802c882a75
EduardoSLuz <edyven100@gmail.com>
2020-05-27 17:16:21 -0400

    Site - Produtos

--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -16,6 +16,21 @@ class Product extends Model{
 		return $sql->select("SELECT * FROM tb_products ORDER BY desproduct");
 
 	}
+
+	public static function checkList($list)
+	{
+
+		foreach ($list as &$row) {
+			
+			$p = new Product();
+			$p->setData($row);
+			$row = $p->getValues();
+
+		}
+
+		return $list;
+
+	}
  	
  	public function save()
 	{
@@ -64,15 +79,21 @@ class Product extends Model{
 	public function checkPhoto()
 	{
 
-		if(file_exists($_SERVER["DOCUMENT_ROOT"].DIRECTORY_SEPARATOR."res".DIRECTORY_SEPARATOR."site".DIRECTORY_SEPARATOR."img".DIRECTORY_SEPARATOR."products".$this->getidproduct().".jpg")){
+		if (file_exists(
+			$_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 
+			"res" . DIRECTORY_SEPARATOR . 
+			"site" . DIRECTORY_SEPARATOR . 
+			"img" . DIRECTORY_SEPARATOR . 
+			"products" . DIRECTORY_SEPARATOR . 
+			$this->getidproduct() . ".jpg"
+			)) {
 
-			$url = "/res/site/img/products/".$this->getidproduct().".jpg";
-		
-		} 
-		else {
-		
+			$url = "/res/site/img/products/" . $this->getidproduct() . ".jpg";
+
+		} else {
+
 			$url = "/res/site/img/product.jpg";
-		
+
 		}
 
 		return $this->setdesphoto($url);
